6925cae11d93064c37e20980980d77ba7d100e42
Velenir <Velenir@users.noreply.github.com>
2016-04-23 19:26:42 +0300

    Initial setup + gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const jade = require('gulp-jade');
+const concat = require('gulp-concat');
+
+
+const src = {
+	scss: 'scss/*.scss',
+	jade: '*.jade',
+	jadePartials: 'partials/*jade',
+	js: ['js/pubsub.js', 'js/*.js']
+};
+
+const dist = {
+	base: 'dist',
+	css: 'dist/css',
+	js: 'dist/js'
+};
+
+// Static Server + watching scss/jade files
+gulp.task('serve', ['sass', 'jade', 'javascript'], function() {
+	browserSync.init({
+		server: {
+			baseDir: dist.base
+		}
+	});
+
+	gulp.watch(src.scss, ['sass']);
+	gulp.watch([src.jade, src.jadePartials], ['jade']);
+	gulp.watch(src.js, ['javascript']);
+});
+
+// Compile sass into CSS
+gulp.task('sass', function() {
+	return gulp.src(src.scss)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(dist.css))
+		.pipe(browserSync.stream({once: true}));
+});
+
+// Compile jade into HTML
+gulp.task('jade', function() {
+	let locals = {};
+
+	return gulp.src(src.jade)
+		.pipe(jade({ locals }))
+		.pipe(gulp.dest(dist.base))
+		.pipe(browserSync.stream({once: true}));
+});
+
+// Concat all jscripts, keeping non-hoisted classes declarations first
+gulp.task('javascript', function() {
+	return gulp.src(src.js)
+		.pipe(concat('bundle.js'))
+		.pipe(gulp.dest(dist.js))
+		.pipe(browserSync.stream({once: true}));
+});
+
+gulp.task('default', ['serve']);
---
+++ js/pubsub.js
@@ -0,0 +1,58 @@
+//events - a super-basic Javascript (publish subscribe) pattern
+
+class EventDispatcher {
+
+	constructor() {
+		this.events = {};
+	}
+
+	// subscribe fn to event
+	on(eventName, fn) {
+		this.events[eventName] ? this.events[eventName].push(fn) : this.events[eventName] = [fn];
+	}
+
+	// unsubscribe fn from event
+	off(eventName, fn) {
+		if (fn == undefined) {
+			// unsubscribe all
+			this.events[eventName] = undefined;
+			return;
+		}
+
+		let events = this.events[eventName];
+		if (events) {
+			let foundIndex = events.indexOf(fn);
+			if (foundIndex !== -1) {
+				events.splice(foundIndex, 1);
+			}
+		}
+	}
+
+	// fire off an event
+	emit(eventName, ...data) {
+		if (this.events[eventName]) {
+			this.events[eventName].forEach(function(fn) {
+				fn(...data);
+			});
+		}
+	}
+
+}
+
+class EventfulClass {
+	constructor() {
+		this.dispatcher = new EventDispatcher();
+	}
+
+	on(...args) {
+		this.dispatcher.on(...args);
+	}
+
+	off(...args) {
+		this.dispatcher.off(...args);
+	}
+
+	emit(...args) {
+		this.dispatcher.emit(...args);
+	}
+}

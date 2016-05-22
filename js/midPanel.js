9c19285988be90a88fd3566496241d18db5ca45a
Velenir <Velenir@users.noreply.github.com>
2016-05-22 19:29:59 +0300

    .midPanel checkboxes emit id:changed on checked state change

---
+++ js/midPanel.js
@@ -0,0 +1,27 @@
+(function () {
+
+	const checkVolume = document.getElementById("check-volume");
+	const checkClockSeconds = document.getElementById("check-seconds");
+	const checkClockFilled = document.getElementById("check-clock-filled");
+	const checkProgress = document.getElementById("check-progress");
+
+	checkVolume.onchange = () => console.log('changed');
+	checkVolume.oninput = () => console.log('input');
+
+	checkVolume.addEventListener('change', function () {
+		eventDispatcher.emit('check-volume:changed', this.checked);
+	});
+
+	checkClockSeconds.addEventListener('change', function () {
+		eventDispatcher.emit('check-seconds:changed', this.checked);
+	});
+
+	checkClockFilled.addEventListener('change', function () {
+		eventDispatcher.emit('check-clock-filled:changed', this.checked);
+	});
+
+	checkProgress.addEventListener('change', function () {
+		eventDispatcher.emit('check-progress:changed', this.checked);
+	});
+
+})();

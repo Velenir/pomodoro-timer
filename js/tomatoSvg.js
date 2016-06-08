263d0476793101da4a99d088f7ce495e9bbffd61
Velenir <Velenir@users.noreply.github.com>
2016-06-08 15:03:34 +0300

    Fixed tomatoSvg reflow not triggering in Firefox

--- js/tomatoSvg.js
+++ js/tomatoSvg.js
@@ -4,10 +4,12 @@ const tomatoSvg = document.querySelector('.all-tomato');
 
 eventDispatcher.on('check-clock-filled:changed', (on, skipTransition) => {
 	if(skipTransition) {
+		// force reflow if necessary
+		if(on !== tomatoSvg.classList.contains('transparent')) return;
 		tomatoSvg.classList.add("no-transition");
 		tomatoSvg.classList.toggle('transparent', !on);
-		// force reflow if necessary
-		if(!on) tomatoSvg.clientWidth;
+		// reflow
+		tomatoSvg.clientWidth;
 		tomatoSvg.classList.remove("no-transition");
 	} else tomatoSvg.classList.toggle('transparent', !on);
 });

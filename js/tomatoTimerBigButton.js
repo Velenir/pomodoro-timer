c3fbf36ed6b57c752e8972d262d4840b73279b08
Velenir <Velenir@users.noreply.github.com>
2016-06-17 23:13:55 +0300

    Update 2016-06-17T20:13:52.056Z

--- js/tomatoTimerBigButton.js
+++
@@ -1,7 +0,0 @@
-// .tomatoTimer
-
-const tomatoTimerBigButton = document.querySelector('.tomatoTimer');
-
-tomatoTimerBigButton.addEventListener('click', () => {
-	eventDispatcher.emit("tomato-big-button-clicked");
-});
